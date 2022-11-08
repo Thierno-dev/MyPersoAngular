cdca4aab8043b69089f3254921be17b08546d136
Thierno-dev <thiernoumar98@gmail.com>
2022-11-08 09:25:11 +0100

    Creation d'un composant

---
+++ FrontEnd/MesPhotos/src/app/ma-photo-statique/ma-photo-statique.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ma-photo-statique',
+  templateUrl: './ma-photo-statique.component.html',
+  styleUrls: ['./ma-photo-statique.component.scss']
+})
+export class MaPhotoStatiqueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
